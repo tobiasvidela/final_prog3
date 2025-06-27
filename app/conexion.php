15d65c5a0c654996833f22ee4772c2744da43d40
tobiasvidela <tobiasvidelaug14@gmail.com>
2025-06-27 11:03:59 -0300

    recomponer para estabilidad

--- app/conexion.php
+++ app/conexion.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $dsn = 'mysql:host=db;dbname=db_final;charset=utf8';
+    $dsn = 'mysql:host=db;dbname=db_final;charset=utf8mb4';
     $pdo = new PDO($dsn, 'appuser', 'appuserpassword', [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
--- init_db.sql
+++ init_db.sql
@@ -222,7 +222,7 @@ INSERT INTO usuarios (username, password, rol) VALUES ('cliente1', 'cliente123',
 INSERT INTO usuarios (username, password, rol) VALUES ('cliente2', 'cliente234', 'cliente');
 INSERT INTO clientes (id_usuario, nombre, apellido, email) 
 VALUES ((SELECT id_usuario FROM usuarios WHERE username = 'cliente1'), 'Juan', 'Perez', 'jperez@email.com'),
-VALUES ((SELECT id_usuario FROM usuarios WHERE username = 'cliente2'), 'Rogelia', 'Gutierrez', 'rguti@email.com');
+        ((SELECT id_usuario FROM usuarios WHERE username = 'cliente2'), 'Rogelia', 'Gutierrez', 'rguti@email.com');
 INSERT INTO productos (nombre, precio, cantidad_stock, proveedor) VALUES ('Laptop', 1000.00, 500, 'TechCorp');
 INSERT INTO productos (nombre, precio, cantidad_stock, proveedor) VALUES ('Teléfono', 500.00, 1050, 'MobileInc');
 INSERT INTO productos (nombre, precio, cantidad_stock, proveedor) VALUES ('Notebook', 120.00, 500, 'TechCorp');
